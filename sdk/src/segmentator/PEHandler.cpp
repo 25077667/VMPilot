6e7b5ee6c8b9b861c1f9d09f3c75ad2377113a84
25077667 <zxc25077667@gmail.com>
2024-05-12 21:25:02 +0800

    feat: Refactor X86Handler implementation
    Remove redundant member variable initialization in the X86Handler::Impl
    constructor. Instead, initialize only the necessary member variables.
    Update make_x86_handler_impl to simplify the creation of X86Handler::Impl
    instance by passing Capstone object directly. Modify doGetNativeFunctions
    to use existing native_functions vector and add a check for empty vector before
    further processing.

---
+++ sdk/src/segmentator/PEHandler.cpp
@@ -0,0 +1,93 @@
+#include <PEHandler.hpp>
+#include <utilities.hpp>
+
+#include <unordered_map>
+
+#include <spdlog/spdlog.h>
+
+using namespace VMPilot::SDK::Segmentator;
+
+namespace {
+namespace detail {
+constexpr std::uint16_t NT_SHORT_NAME_LEN = 8;
+
+struct PEParser {
+    // Placeholder for PE parsing, not implemented
+    // TODO: Add third party library for PE parsing
+};
+
+struct ImageSectionHeader {
+    std::uint8_t Name[NT_SHORT_NAME_LEN];
+    union {
+        std::uint32_t PhysicalAddress;
+        std::uint32_t VirtualSize;
+    } Misc;
+    std::uint32_t VirtualAddress;
+    std::uint32_t SizeOfRawData;
+    std::uint32_t PointerToRawData;
+    std::uint32_t PointerToRelocations;
+    std::uint32_t PointerToLinenumbers;
+    std::uint16_t NumberOfRelocations;
+    std::uint16_t NumberOfLinenumbers;
+    std::uint32_t Characteristics;
+};
+}  // namespace detail
+}  // namespace
+
+struct PEFileHandlerStrategy::Impl {
+    detail::PEParser pe_handler;
+    std::unordered_map<std::string, detail::ImageSectionHeader> section_table;
+    uint64_t vmp_begin_addr = -1;
+    uint64_t vmp_end_addr = -1;
+    uint64_t text_base_addr = -1;
+};
+
+std::unique_ptr<PEFileHandlerStrategy::Impl>
+VMPilot::SDK::Segmentator::make_pe_impl(const std::string& file_name) {
+    auto impl = std::make_unique<PEFileHandlerStrategy::Impl>();
+    // Placeholder for PE parsing, not implemented
+
+    return impl;
+}
+
+PEFileHandlerStrategy::PEFileHandlerStrategy(const std::string& file_name)
+    : pImpl(make_pe_impl(file_name)) {
+    spdlog::error("PEFileHandlerStrategy not implemented");
+}
+
+PEFileHandlerStrategy::~PEFileHandlerStrategy() {}
+
+std::pair<uint64_t, uint64_t>
+PEFileHandlerStrategy::doGetBeginEndAddr() noexcept {
+    spdlog::error("PEFileHandlerStrategy::doGetBeginEndAddr not implemented");
+    return std::make_pair(-1, -1);
+}
+
+std::vector<uint8_t> PEFileHandlerStrategy::doGetTextSection() noexcept {
+    spdlog::error("PEFileHandlerStrategy::doGetTextSection not implemented");
+    return std::vector<uint8_t>();
+}
+
+uint64_t PEFileHandlerStrategy::doGetTextBaseAddr() noexcept {
+    spdlog::error("PEFileHandlerStrategy::doGetTextBaseAddr not implemented");
+    return -1;
+}
+
+std::pair<uint64_t, uint64_t>
+PEFileHandlerStrategy::doGetBeginEndAddrIntl() noexcept {
+    spdlog::error(
+        "PEFileHandlerStrategy::doGetBeginEndAddrIntl not implemented");
+    return std::make_pair(-1, -1);
+}
+
+std::vector<uint8_t> PEFileHandlerStrategy::doGetTextSectionIntl() noexcept {
+    spdlog::error(
+        "PEFileHandlerStrategy::doGetTextSectionIntl not implemented");
+    return std::vector<uint8_t>();
+}
+
+std::vector<uint8_t> PEFileHandlerStrategy::getSectionData(
+    const std::string& sectionName) noexcept {
+    spdlog::error("PEFileHandlerStrategy::getSectionData not implemented");
+    return std::vector<uint8_t>();
+}
